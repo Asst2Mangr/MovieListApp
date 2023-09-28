e1b8e031fe27155ec43a08a04811eb03d5dc5e32
Asst2Mangr <124299473+Asst2Mangr@users.noreply.github.com>
2023-09-28 09:48:49 -0500

    finished step 12

---
+++ Models/Movie.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.Contracts;
+
+namespace MovieListApp.Models
+{
+    public class Movie
+    {
+        //EF Core will configure the database to generate this value
+        public int MovieID { get; set; }
+        [Required(ErrorMessage = "Please enter a name.")]
+        public string Name { get; set; }
+        [Required(ErrorMessage ="Please enter a year.")]
+        [Range(1889, 2999, ErrorMessage ="Year must be after 1889.")]
+        public int? Year { get; set; }
+        [Required(ErrorMessage ="Please eater a rating.")]
+        [Range(1, 5, ErrorMessage ="Rating must be between 1 and 5.")]
+        public int? Rating { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using MovieListApp.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<MovieContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("MovieContext")));
 
 var app = builder.Build();
 
@@ -22,6 +26,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}/{slug?}");
 
 app.Run();
